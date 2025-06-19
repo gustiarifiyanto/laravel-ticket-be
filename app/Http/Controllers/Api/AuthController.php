1c1833e42c1f7dee4200aeb6a0589bbfc88cad1b
gustiarifiyanto <gusti.arifiyanto@gmail.com>
2025-06-19 22:54:28 +0700

    Membuat file AuthController dan install resources google apiclient

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    //function for register
+    public function register(Request $request)
+    {
+        // Validate the request data
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users,email',
+            'password' => 'required|string|min:5|confirmed',
+        ]);
+
+        // Create the user
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        // Return a response
+        return response()->json([
+            'status' => 'success',
+            'message' => 'User registered successfully',
+            'data' => $user,
+        ], 201);
+    }
+}
