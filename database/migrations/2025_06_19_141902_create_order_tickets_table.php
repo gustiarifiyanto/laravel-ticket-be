4c983572002f2626923658535b2ef959121666ba
gustiarifiyanto <gusti.arifiyanto@gmail.com>
2025-06-19 21:26:44 +0700

    Membuat tabel sku, ticket, order, dan order_ticket

---
+++ database/migrations/2025_06_19_141902_create_order_tickets_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order_tickets', function (Blueprint $table) {
+            $table->id();
+            // order_id - foreign key to orders table
+            $table->foreignId('order_id')->constrained()->onDelete('cascade');
+            // ticket_id - foreign key to tickets table
+            $table->foreignId('ticket_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_tickets');
+    }
+};
